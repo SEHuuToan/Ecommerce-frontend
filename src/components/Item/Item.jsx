6f983cff911fd5b5e5b720761f77adf9728f21b7
huutoanrobotic <huutoanit2023@gmail.com>
2024-05-16 11:40:59 +0700

    done popular motocycle

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -5,15 +5,18 @@ const Item = (props) => {
     return(
         <div className="item">
                 <img src={props.image} alt="motocycle_image" />
-                <p>{props.name}</p>
-                <p>Odo: {props.odo} Km</p>
-                <p>Color: {props.color}</p>
-                <p>Model: {props.model}</p>
-                <div className="item-price">
-                    <div className="item-price-bike">
-                       Price: {props.price} $
-                    </div>
-                </div>         
+                <div className="item_description">
+                    <div className="item_name">{props.name}</div>
+                    <p>Odo: {props.odo} Km</p>
+                    <p>Color: {props.color}</p>
+                    <p>Model: {props.model}</p>
+                    <div className="item-price">
+                        <div className="item-price-bike">
+                        Price: {props.price} $
+                        </div>
+                    </div>   
+                </div>
+                   
         </div>
     );
 }
