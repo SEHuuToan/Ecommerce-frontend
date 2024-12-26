ba5f586a4d61e35c2de0931a4b38a0a66d531719
HuuToan <huutoanit2023@gmail.com>
2024-12-26 22:08:01 +0700

    update lost file build

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,5 +1,5 @@
 import HeroPart from '../components/hero_part/HeroPart.jsx';
-import PopularPart from "../components/popular/PopularPart.jsx";
+import PopularPart from "../components/popular_part/PopularPart.jsx";
 import Blog_HomePage from "../components/blog_homepage/Blog_HomePage";
 import HomeContent from '../components/home_content/HomeContent';
 import './css/Shop.css';
