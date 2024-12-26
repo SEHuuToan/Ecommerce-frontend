37c8597f143e31997a35b201b951e1f7299461a7
HuuToan <huutoanit2023@gmail.com>
2024-12-26 21:51:58 +0700

    update build 3th

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  alias: {
+    '@components': path.resolve(__dirname, './src/components'),
+  },
 })
