651359ba1c43ee586b2de3156f0c291b0cd8e95d
huutoan24092000 <138637847+SEHuuToan@users.noreply.github.com>
2024-12-29 10:37:25 +0700

    update(axiosUtils): change logic get API from back-end service

--- src/utils/axiosUtils.js
+++ src/utils/axiosUtils.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 // const HOST = 'http://192.168.2.224:4000/'
-const HOST = import.meta.env.VITE_BASE_URL_LOCAL;
+const HOST = import.meta.env.BACK_END_URL_API;
 
 const axiosGet = async (url) => {
-    const res = await axios.get(`${HOST}api/products/${url}`)
+    const res = await axios.get(`${HOST}/api/products/${url}`)
     return res;
 }
 const axiosGetWithParams = async (url, params) => {
